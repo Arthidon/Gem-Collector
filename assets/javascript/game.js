b43521329a08e5f90cbcb345e72e94d05773c6fd
Anthony Yonta <atischillin@gmail.com>
2019-11-12 13:21:20 -0500

    added label to number to hit, fixed some typos

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -55,10 +55,10 @@ function reset(){
   console.log("Gem2 " + gem2);
   console.log("Gem3 " + gem3);
   console.log("Gem4 " + gem4);
-  console.log("GemSum " + gemSum);
+  console.log("GemSum " + "Number to hit: " gemSum);
 
   $("#randomNumber").text (rand);
-  $("#total").text ( "Gem Guess Numer " + gemSum);
+  $("#total").text ( "Gem Guess Number: " + gemSum);
   
   return  { rand : rand, gem1 : gem1 , gem2 : gem2, gem3 : gem3, gem4 : gem4, gemSum : gemSum };
 
